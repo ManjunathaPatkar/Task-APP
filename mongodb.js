bdb9792668bb33ed52c670a658faa686d53d4e70
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-12 21:18:48 +0530

    inserting many

--- mongodb.js
+++ mongodb.js
@@ -3,18 +3,40 @@ const MongoClient=mongodb.MongoClient
 const connectionURL ='mongodb://127.0.0.1:27017'
 const databaseName ='task-manager'
 
-MongoClient.connect(connectionURL,{ useNewUrlParser:true},(err,client)=>{
-    if(err){
-        return console.log("Error")
+
+// MongoClient.connect(connectionURL,{ useNewUrlParser:true},(err,client)=>{
+//     if(err){
+//         return console.log("Error")
+//     }
+//     const db=client.db(databaseName)
+
+//     db.collection('users').insertOne({
+//         name:'Manjunatha Patkar',
+//         age:21
+//     },(err,result)=>{
+//         if(err){
+//             return console.log("error while inserting")
+//         }
+//         console.log(result.ops)
+//     })
+// })
+MongoClient.connect(connectionURL,{useNewUrlParser:true},(error,client)=>{
+    if(error){
+        return console.log("Error connecting to databse")
     }
     const db=client.db(databaseName)
-
-    db.collection('users').insertOne({
-        name:'Manjunatha Patkar',
-        age:21
-    },(err,result)=>{
+    db.collection('users').insertMany([
+        {
+            name:'manja',
+            age:21
+        },
+        {
+            name:'admin',
+            age:23
+        }
+    ],(err,result)=>{
         if(err){
-            return console.log("error while inserting")
+            return console.log("error inserting value")
         }
         console.log(result.ops)
     })
