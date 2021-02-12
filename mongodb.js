b3c01f13806d76df443af2b0c049b6104760fc21
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-12 21:07:20 +0530

    insert one document to mongodb

--- mongodb.js
+++ mongodb.js
@@ -2,14 +2,20 @@ const mongodb=require('mongodb')
 const MongoClient=mongodb.MongoClient
 const connectionURL ='mongodb://127.0.0.1:27017'
 const databaseName ='task-manager'
-MongoClient.connect(connectionURL,{useNewUrlParser : true} ,(error,client)=>{
-    if(error){
-        return console.log('Unable to connect to fatabase ',error)
+
+MongoClient.connect(connectionURL,{ useNewUrlParser:true},(err,client)=>{
+    if(err){
+        return console.log("Error")
     }
-    // console.log("connected successsfully",client)
     const db=client.db(databaseName)
+
     db.collection('users').insertOne({
-        name:'Manjunatha',
-        age:20
+        name:'Manjunatha Patkar',
+        age:21
+    },(err,result)=>{
+        if(err){
+            return console.log("error while inserting")
+        }
+        console.log(result.ops)
     })
 })
