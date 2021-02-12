a0c5d9388ba47b34ada1dafaad29097293fc5a03
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-12 16:01:25 +0530

    added connectio to mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,2 +1,10 @@
 const mongodb=require('mongodb')
 const MongoClient=mongodb.MongoClient
+const connectionURL ='mongodb://127.0.0.1:27017'
+const databaseName ='task-manager'
+MongoClient.connect(connectionURL,{useNewUrlParser : true} ,(error,client)=>{
+    if(error){
+        return console.log('Unable to connect to fatabase ',error)
+    }
+    console.log("connected successsfully",client)
+})
