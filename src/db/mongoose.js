681af73ad57947a4e00ed45705e8c8129bc3fc07
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-13 17:27:12 +0530

    adding value to db using mongoose task

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,17 +4,35 @@
      useCreateIndex:true
  })
 
-const User = mongoose.model("User",{
-    name:{
+// const User = mongoose.model("User",{
+//     name:{
+//         type:String
+//     },
+//     age:{
+//         type:Number
+//     }
+// })
+// const me=new User({
+//     name:"Manja",
+//     age:21
+// })
+// me.save().then((me)=>{
+//     console.log(me)
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+const Task=mongoose.model('Tasks',{
+    description:{
         type:String
     },
-    age:{
-        type:Number
+    completed:{
+        type:Boolean
     }
 })
-const me=new User({
-    name:"Manja",
-    age:21
+const me=new Task({
+    description:"clean macbook its avvvv dusty",
+    completed:false
 })
 me.save().then((me)=>{
     console.log(me)
