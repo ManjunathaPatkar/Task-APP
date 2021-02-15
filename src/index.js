9c4bf67719ebebf831209efaee5e8dad6d4495c3
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-15 20:39:26 +0530

    Added post

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express=require('express')
 
-const app = express
+const app = express()
+
 const port = process.env.PORT||3000
+
+app.post('/users',(req,res)=>{
+    res.send("Testing")
+})
+
 app.listen(port,()=>{
     console.log("Server is up on "+port)
 })
