33c388b598481a5880ac85fea1f44d13022a98c0
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-15 21:30:00 +0530

    Done for users

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,49 +1,49 @@
  const mongoose = require('mongoose')
- const validator=require('validator')
+//  const validator=require('validator')
  mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
      useNewUrlParser:true,
      useCreateIndex:true
  })
 
-const User = mongoose.model("User",{
-    name:{
-        type:String,
-        required:true,
-        default:'Anonymous',
-        trim:true
-    },
-    age:{
-        type:Number,
-        default:0,
-        validate(value){
-            if(value<0){
-                throw new Error("Age cannot be negative")
-            }
-        }
-    },
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        lowercase:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error("Email invalid")
-            }
-        }
-    },
-    password:{
-        type:String,
-        required:true,
-        minlength:7,
-        trim:true,
-        validate(value){
-            if (value.toLowerCase().includes('password')){
-                throw new Error("Password should not contain password")
-            }
-        }
-    }
-})
+// const User = mongoose.model("User",{
+//     name:{
+//         type:String,
+//         required:true,
+//         default:'Anonymous',
+//         trim:true
+//     },
+//     age:{
+//         type:Number,
+//         default:0,
+//         validate(value){
+//             if(value<0){
+//                 throw new Error("Age cannot be negative")
+//             }
+//         }
+//     },
+//     email:{
+//         type:String,
+//         required:true,
+//         trim:true,
+//         lowercase:true,
+//         validate(value){
+//             if(!validator.isEmail(value)){
+//                 throw new Error("Email invalid")
+//             }
+//         }
+//     },
+//     password:{
+//         type:String,
+//         required:true,
+//         minlength:7,
+//         trim:true,
+//         validate(value){
+//             if (value.toLowerCase().includes('password')){
+//                 throw new Error("Password should not contain password")
+//             }
+//         }
+//     }
+// })
 // const me=new User({
 //     name:"    Manja Patkar   ",
 //     age:23,
@@ -56,23 +56,23 @@ const User = mongoose.model("User",{
 //     console.log(err)
 // })
 
-const Task=mongoose.model('Tasks',{
-    description:{
-        type:String,
-        required:true,
-        trim:true
-    },
-    completed:{
-        type:Boolean,
-        default:false
-    }
-})
-const me=new Task({
-    description:"clean macbook its avvvv dusty",
-    completed:false
-})
-me.save().then((me)=>{
-    console.log(me)
-}).catch((err)=>{
-    console.log(err)
-})
+// const Task=mongoose.model('Tasks',{
+//     description:{
+//         type:String,
+//         required:true,
+//         trim:true
+//     },
+//     completed:{
+//         type:Boolean,
+//         default:false
+//     }
+// })
+// const me=new Task({
+//     description:"clean macbook its avvvv dusty",
+//     completed:false
+// })
+// me.save().then((me)=>{
+//     console.log(me)
+// }).catch((err)=>{
+//     console.log(err)
+// })
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const express=require('express')
 
+require("./db/mongoose")
+
+const User = require("./models/user")
+
 const app = express()
 
 const port = process.env.PORT||3000
 
+app.use(express.json())
+
 app.post('/users',(req,res)=>{
-    res.send("Testing")
+    const user=new User(req.body)
+    user.save().then((user)=>{
+        res.send(user)
+    }).catch((err)=>{
+        res.status(404).send(err)
+    })
+    
 })
 
 app.listen(port,()=>{
