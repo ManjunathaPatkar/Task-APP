8f6a8271f147998945ad11e2ff96256c269a1a2e
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-17 19:44:18 +0530

    update promise chaining 1:

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -14,12 +14,12 @@ const User=require("../src/models/user")
 //     console.log(err)
 // })
 
-const findandUpdateandCount=async (id,age)=>{
+const UpdateAgeAndCount=async (id,age)=>{
     const user=await User.findByIdAndUpdate(id,{age})
     const count=await User.countDocuments({age:23})
     return count
 }
-findandUpdateandCount('6027d72ad73e72a9c947e54a',30).then((count)=>{
+UpdateAgeAndCount('6027d72ad73e72a9c947e54a',30).then((count)=>{
     console.log(count)
 }).catch((err)=>{
     console.log(err)
