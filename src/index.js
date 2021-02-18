38ca1684146c83305c7cc4191e6a907d347c7de4
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-18 19:29:20 +0530

    task also done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ require("./db/mongoose")
 
 const UserRouter=require("../src/routers/user")
 
+const  TaskRouter=require("../src/routers/task")
+
 const User = require("./models/user")
 
 const Task=require("./models/task")
@@ -14,55 +16,8 @@ const port = process.env.PORT||3000
 
 app.use(express.json())
 app.use(UserRouter)
+app.use(TaskRouter)
 
-app.patch("/tasks/:id",async(req,res)=>{
-    const allowedupdates=['description','completed']
-    const updates=Object.keys(req.body)
-    const isValidOperation= updates.every((update)=>{
-        return (allowedupdates.includes(update))
-    })
-    if(!isValidOperation){
-        return res.status(400).send({
-            "error": "Invalid updates "
-        })
-    }
-    try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true,runValidators:true})
-        if(!task){
-            res.status(404).send()
-        }
-        res.send(task)
-    }
-    catch(err){
-        res.status(400).send()
-    }
-})
-
-app.delete("/users/:id",async(req,res)=>{
-    try{
-        const user=await User.findByIdAndDelete(req.params.id)
-        if(!user){
-            res.status(404).send()
-        }
-        res.send(user)
-    }
-    catch(err){
-        res.status(400).send()
-    }
-})
-
-app.delete("/tasks/:id",async(req,res)=>{
-    try{
-        const task = await Task.findByIdAndDelete(req.params.id)
-        if(!task){
-            res.status(404).send()
-        }
-        res.send(task)
-    }
-    catch(err){
-        res.status(400).send()
-    }
-})
 
 app.listen(port,()=>{
     console.log("Server is up on "+port)
--- src/routers/user.js
+++ src/routers/user.js
@@ -37,41 +37,6 @@ router.get('/users/:id', async (req, res) => {
     }
 })
 
-router.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-    try {
-        await task.save()
-        res.status(201).send(task)
-    }
-    catch (err) {
-        res.status(404).send()
-    }
-})
-
-router.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find({})
-        res.send(tasks)
-    }
-    catch (err) {
-        res.status(500).send()
-    }
-})
-
-router.get('/tasks/:id', async (req, res) => {
-    const id = req.params.id
-    try {
-        const task = await Task.findById(id)
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    }
-    catch (err) {
-        res.status(500).send()
-    }
-})
-
 router.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedupdates = ['name', 'email', 'password', 'age']
@@ -84,6 +49,19 @@ router.patch('/users/:id', async (req, res) => {
 
     try {
         const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!user) {
+            res.status(404).send()
+        }
+        res.send(user)
+    }
+    catch (err) {
+        res.status(400).send()
+    }
+})
+
+router.delete("/users/:id", async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
         if (!user) {
             res.status(404).send()
         }
