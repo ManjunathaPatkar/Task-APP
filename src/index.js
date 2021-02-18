0eeae707285bead9e494b0c7c1fa44d84b15f99e
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-18 19:12:45 +0530

    moved all user route code to seperate file to make code look more readable

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express=require('express')
 
 require("./db/mongoose")
 
+const UserRouter=require("../src/routers/user")
+
 const User = require("./models/user")
 
 const Task=require("./models/task")
@@ -11,99 +13,7 @@ const app = express()
 const port = process.env.PORT||3000
 
 app.use(express.json())
-
-app.post('/users',async(req,res)=>{
-    const user=new User(req.body)
-    try{
-        await user.save()
-        res.status(201).send(user)
-    }
-    catch(err){
-        res.status(404).send(err)
-    }
-})
-
-app.get('/users',async (req,res)=>{
-    try {
-        const users=await User.find({})
-        res.send(users)
-    }
-    catch(err){
-        res.status(500).send()
-    }
-})
-
-app.get('/users/:id',async (req,res)=>{
-    const id=req.params.id
-    try
-    {
-        const user=await User.findById(id)
-        if(!user){
-            return res.status(404).send()
-        }
-        res.send(user)
-    }
-    catch(err){
-        res.status(500).send()
-    }
-})
-
-app.post('/tasks',async(req,res)=>{
-    const task=new Task(req.body)
-    try{
-        await task.save()
-        res.status(201).send(task)
-    }
-    catch(err){
-        res.status(404).send()
-    }
-})
-
-app.get('/tasks',async(req,res)=>{
-    try{
-        const tasks=await Task.find({})
-        res.send(tasks)
-    }
-    catch(err){
-        res.status(500).send()
-    }
-})
-
-app.get('/tasks/:id',async(req,res)=>{
-    const id=req.params.id
-    try{
-        const task=await Task.findById(id)
-        if(!task){
-            return res.status(404).send()
-        }
-        res.send(task)
-    }
-    catch(err){
-        res.status(500).send()
-    }
-})
-
-app.patch('/users/:id', async(req,res)=>{
-    const updates=Object.keys(req.body)
-    const allowedupdates=['name','email','password','age']
-    const isValidOperation=updates.every((update)=>{
-        return allowedupdates.includes(update)
-    })
-    if(!isValidOperation){
-        return res.status(400).send({'error': 'Invalid updates'})
-    }
-
-    try {
-        const user=await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})
-        if(!user){
-            res.status(404).send()
-        }
-        res.send(user)
-    }
-    catch(err){
-        res.status(400).send()
-    }
-})
+app.use(UserRouter)
 
 app.patch("/tasks/:id",async(req,res)=>{
     const allowedupdates=['description','completed']
