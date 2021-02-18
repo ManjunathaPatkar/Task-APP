1627fad7ca3a2e273f46dc09cf1551ee113332ee
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-18 15:36:34 +0530

    Async wait done for updating values

--- src/index.js
+++ src/index.js
@@ -105,6 +105,29 @@ app.patch('/users/:id', async(req,res)=>{
     }
 })
 
+app.patch("/tasks/:id",async(req,res)=>{
+    const allowedupdates=['description','completed']
+    const updates=Object.keys(req.body)
+    const isValidOperation= updates.every((update)=>{
+        return (allowedupdates.includes(update))
+    })
+    if(!isValidOperation){
+        return res.status(400).send({
+            "error": "Invalid updates "
+        })
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true,runValidators:true})
+        if(!task){
+            res.status(404).send()
+        }
+        res.send(task)
+    }
+    catch(err){
+        res.status(400).send()
+    }
+})
+
 app.listen(port,()=>{
     console.log("Server is up on "+port)
 })
