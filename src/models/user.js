5a882c1a0923bb84d779032008f2f8ffed4c8758
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-21 11:42:51 +0530

    testing whether shema pre is invoked

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-const User = mongoose.model("User", {
+const Userschema = mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -40,4 +40,12 @@ const User = mongoose.model("User", {
         }
     }
 })
+
+Userschema.pre('save',async function(next){
+    const user=this
+    console.log('just before saving')
+    next()
+})
+
+const User = mongoose.model("User", Userschema)
 module.exports=User
